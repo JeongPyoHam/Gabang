565dbd85a04102d8128434c36da08ba6ccd9208d
JP Ham <jeongham@microsoft.com>
2015-10-19 21:08:20 -0700

    Add missing file

---
+++ Gabang/Controls/TreeGridExpanderColumn.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace Gabang.Controls
+{
+    public class TreeGridExpanderColumn : DataGridTemplateColumn
+    {
+        public virtual BindingBase DepthBinding { get; set; }
+
+        protected override FrameworkElement GenerateElement(DataGridCell cell, object dataItem)
+        {
+            var presenter = new TreeGridExpanderPresenter();
+
+            BindingOperations.SetBinding(presenter, ContentControl.ContentProperty, new Binding());
+
+            if (DepthBinding != null)
+            {
+                BindingOperations.SetBinding(presenter, TreeGridExpanderPresenter.DepthProperty, DepthBinding);
+            }
+
+            presenter.ContentTemplate = this.CellTemplate;
+            presenter.ContentTemplateSelector = this.CellTemplateSelector;
+
+            return presenter;
+        }
+    }
+}
---
+++ Gabang/Controls/TreeGridExpanderPresenter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Gabang.Controls
+{
+    public class TreeGridExpanderPresenter : ContentControl
+    {
+        static TreeGridExpanderPresenter()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(TreeGridExpanderPresenter), new FrameworkPropertyMetadata(typeof(TreeGridExpanderPresenter)));
+        }
+
+        public TreeGridExpanderPresenter()
+        {
+        }
+
+        public static DependencyProperty DepthProperty = DependencyProperty.Register("Depth", typeof(int), typeof(TreeGridExpanderPresenter));
+        public int Depth
+        {
+            get { return (int)GetValue(DepthProperty); }
+            set { SetValue(DepthProperty, value); }
+        }
+    }
+}
