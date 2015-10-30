dcee79654db0a6df99d2813f60df4fe7eb358582
JP Ham <jeongham@microsoft.com>
2015-10-30 15:39:04 -0700

    Temporary checkin

---
+++ Gabang/Controls/VirtualizingGrid/JointGrid.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace Gabang.Controls {
+    /// <summary>
+    /// ItemsControl for two dimentional collection; collection of collection
+    /// Need to put some restriction on IList<IList<T>>
+    /// 
+    /// GridLine
+    /// Row Header
+    /// Column Header
+    /// </summary>
+    public class JointGrid : MultiSelector {
+
+        static JointGrid() {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(JointGrid), new FrameworkPropertyMetadata(typeof(JointGrid)));
+        }
+
+        public JointGrid() {
+        }
+
+        #region public
+
+        public IList<object> ColumnHeaderProvider { get; set; }
+
+        public IList<object> RowHeaderProvider { get; set; }
+
+        #endregion
+
+        #region override
+
+        protected override DependencyObject GetContainerForItemOverride() {
+            return new JointGridRow();
+        }
+
+        protected override void OnItemsSourceChanged(IEnumerable oldValue, IEnumerable newValue) {
+            if (newValue is IList<IList<object>>) {
+                throw new NotSupportedException($"JointGrid supports only joint IList collection of which type is {typeof(IList<IList<object>>)}");
+            }
+
+            base.OnItemsSourceChanged(oldValue, newValue);
+        }
+
+        #endregion override
+    }
+}
---
+++ Gabang/Controls/VirtualizingGrid/VirtualizingGridPanel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace Gabang.Controls {
+    /// <summary>
+    /// a panel(vertical) of row panel(horizontal)
+    /// send horizontal scroll to each row panel
+    /// 
+    /// item generation (virtualization) should be synchronized, too
+    /// column item layout should be synchronized, too
+    /// 
+    /// </summary>
+    public class VirtualizingGridPanel : VirtualizingStackPanel {
+        protected override Size MeasureOverride(Size availableSize) {
+            EnsureItemContainerGenerator();
+
+            IItemContainerGenerator itemGenerator = this.ItemContainerGenerator;
+            if (itemGenerator != null) {
+                GeneratorPosition position = itemGenerator.GeneratorPositionFromIndex(0);
+
+                using (itemGenerator.StartAt(position, GeneratorDirection.Forward)) {
+                    var item = itemGenerator.GenerateNext();
+                }
+            }
+
+            return base.MeasureOverride(availableSize);
+        }
+
+        protected override Size ArrangeOverride(Size finalSize) {
+            return base.ArrangeOverride(finalSize);
+        }
+
+        private void EnsureItemContainerGenerator() {
+            var children = this.Children;
+        }
+    }
+}
