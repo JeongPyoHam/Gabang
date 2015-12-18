e406ad6824c81973971c8fb3708995e53fc5257d
JP Ham <jeongham@microsoft.com>
2015-12-18 09:27:09 -0800

    GridLine for Visual

--- Gabang/Controls/GridPanel/GridPanel2.cs
+++ Gabang/Controls/GridPanel/GridPanel2.cs
@@ -9,12 +9,14 @@ using System.Windows;
 using System.Windows.Media;
 
 namespace Gabang.Controls {
+
     public class GridPanel2 : FrameworkElement {
         private double[] _xPositions;
         private double[] _yPositions;
         private double[] _width;
         private double[] _height;
 
+        private DrawingVisual _gridLine;
         private VisualCollection _visualChildren;
 
         public GridPanel2() {
@@ -26,6 +28,14 @@ namespace Gabang.Controls {
 
         public int ColumnCount { get { return 50; } }
 
+        public double MinItemWidth { get { return 20.0; } }
+
+        public double MinItemHeight { get { return 10.0; } }
+
+        public double GridLineThickness { get { return 1.0; } }
+
+        public Brush GridLineBrush { get { return Brushes.Black; } }
+
         public void AddChileren() {
             Stopwatch watch = Stopwatch.StartNew();
 
@@ -36,32 +46,19 @@ namespace Gabang.Controls {
             _width = new double[ColumnCount];
             _height = new double[RowCount];
 
+            _gridLine = new DrawingVisual();
+
             for (int r = 0; r < RowCount; r++) {
                 for (int c = 0; c < ColumnCount; c++) {
                     var visual = new GridPanel2Visual();
                     visual.Row = r;
                     visual.Column = c;
-                    visual.Text = string.Format("{0}:{1}", r.ToString(), c.ToString());
-
-                    DrawingContext dc = visual.RenderOpen();
-                    try {
-                        dc.DrawText(visual.GetFormattedText(), new Point(0, 0));
-                    } finally {
-                        dc.Close();
-                    }
-
-                    _width[c] = Math.Max(_width[c], visual.ContentBounds.Width);
-                    _height[r] = Math.Max(_height[r], visual.ContentBounds.Height);
 
                     _visualChildren.Add(visual);
                 }
             }
 
-            ComputePositions();
-
-            foreach (GridPanel2Visual visual in _visualChildren) {
-                visual.Transform = new TranslateTransform(_xPositions[visual.Column], _yPositions[visual.Row]);
-            }
+            RefreshChildren();
 
             Trace.WriteLine(string.Format("Add:{0}", watch.ElapsedMilliseconds));
         }
@@ -75,29 +72,67 @@ namespace Gabang.Controls {
 
                 DrawingContext dc = visual.RenderOpen();
                 try {
-                    dc.DrawText(visual.GetFormattedText(), new Point(0, 0));
+                    var formattedText = visual.GetFormattedText();
+                    dc.DrawText(formattedText, new Point(0, 0));
                 } finally {
                     dc.Close();
                 }
                 int r = visual.Row;
                 int c = visual.Column;
 
-                _width[c] = Math.Max(_width[c], visual.ContentBounds.Width);
-                _height[r] = Math.Max(_height[r], visual.ContentBounds.Height);
+                _width[c] = Math.Max(_width[c], visual.ContentBounds.Width + GridLineThickness);
+                _height[r] = Math.Max(_height[r], visual.ContentBounds.Height + GridLineThickness);
             }
 
             ComputePositions();
 
             foreach (GridPanel2Visual visual in _visualChildren) {
                 var transform = visual.Transform as TranslateTransform;
-
-                transform.X = _xPositions[visual.Column];
-                transform.Y = _yPositions[visual.Row];
+                if (transform == null) {
+                    visual.Transform = new TranslateTransform(_xPositions[visual.Column], _yPositions[visual.Row]);
+                } else {
+                    transform.X = _xPositions[visual.Column];
+                    transform.Y = _yPositions[visual.Row];
+                }
             }
+
+            DrawGridLine();
 
             generation++;
 
             Trace.WriteLine(string.Format("Refesh:{0}", watch.ElapsedMilliseconds));
+        }
+
+        private void DrawGridLine() {
+            if (_gridLine == null) return;
+            DrawingContext drawingContext = _gridLine.RenderOpen();
+            DoubleCollection xCollection = new DoubleCollection();
+            DoubleCollection yCollection = new DoubleCollection();
+            try {
+                // vertical line
+                for (int i = 0; i < ColumnCount; i++) {
+                    Rect rect = new Rect(new Size(GridLineThickness, RenderSize.Height));
+                    rect.X = _xPositions[i] + _width[i] - GridLineThickness;
+
+                    drawingContext.DrawRectangle(GridLineBrush, null, rect);
+
+                    xCollection.Add(_xPositions[i]);
+                }
+
+                // horizontal line
+                for (int i = 0; i < RowCount; i++) {
+                    Rect rect = new Rect(new Size(RenderSize.Width, GridLineThickness));
+                    rect.Y = _yPositions[i] + _height[i] - GridLineThickness;
+
+                    drawingContext.DrawRectangle(GridLineBrush, null, rect);
+                    yCollection.Add(_yPositions[i]);
+                }
+
+                _gridLine.XSnappingGuidelines = xCollection;
+                _gridLine.YSnappingGuidelines = yCollection;
+            } finally {
+                drawingContext.Close();
+            }
         }
 
         private void ComputePositions() {
@@ -112,6 +147,12 @@ namespace Gabang.Controls {
                 width += _width[i];
                 _xPositions[i + 1] = width;
             }
+        }
+
+        protected override void OnRenderSizeChanged(SizeChangedInfo sizeInfo) {
+            base.OnRenderSizeChanged(sizeInfo);
+
+            DrawGridLine();
         }
 
         protected override Size MeasureOverride(Size availableSize) {
@@ -129,21 +170,21 @@ namespace Gabang.Controls {
             var size = base.ArrangeOverride(finalSize);
 
             Trace.WriteLine(string.Format("Arrange:{0}", watch.ElapsedMilliseconds));
-
             return size;
         }
 
         protected override int VisualChildrenCount {
             get {
-                return _visualChildren.Count;
+                if (_visualChildren.Count == 0) return 0;
+                return _visualChildren.Count + 1;
             }
         }
 
         protected override Visual GetVisualChild(int index) {
-            if (index < 0 || index >= _visualChildren.Count)
+            if (index < 0 || index >= _visualChildren.Count + 1)
                 throw new ArgumentOutOfRangeException("index");
-
-            return _visualChildren[index];
+            if (index == 0) return _gridLine;
+            return _visualChildren[index - 1];
         }
     }
 
